1a8b4f83ccce3babf9482a4b99a2a388b2303c22
lopespkm <59057440+lopespkm@users.noreply.github.com>
2025-07-29 17:04:33 -0300

    frontend

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.ultrapanel.shop',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ultrapanel.shop',
+        pathname: '/**', // permite qualquer imagem do domínio
+      },
+      {
+        protocol: 'https',
+        hostname: 'sucessogrupo.shop',
+        pathname: '/imagemsras/**', // ajuste conforme necessário
+      },
+    ],
+  },
+};
+
+export default nextConfig;
